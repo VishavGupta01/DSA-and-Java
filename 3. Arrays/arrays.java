5c7421f8873dee9e13e1e3b8c8d9c567a966290e
Vishav Gupta <singlasavita91@gmail.com>
2024-05-08 17:06:03 +0530

    Kadane's Algo Special Case Note

--- 3. Arrays/arrays.java
+++ 3. Arrays/arrays.java
@@ -193,7 +193,7 @@ public class arrays {
         int array[] = {-2,-3,4,-1,-2,1,5,-3};
         int cs = 0;
         int ms = Integer.MIN_VALUE;
-        //All Negative Special Case:
+        //All Negative Special Case: (Under Work, Incomplete)
         // int min = Integer.MIN_VALUE;
         // for(int i = 0; i < array.length; i++) {
         //     if (array[i] >= 0) {
