ef1c35ada16745ca4daa9718cb16a3b0c27d2123
Vishav Gupta <singlasavita91@gmail.com>
2024-05-08 21:36:53 +0530

    Separated Kadane's Algorithm
    Included all negative elements case.

---
+++ 3. Arrays/kadanes.java
@@ -0,0 +1,36 @@
+public class kadanes {
+    public static void main(String args[]) {
+        int array[] = {-1,-2,-3,-4,5}; // negative Case
+        //int array[] = {-2,-3,4,-1,-2,1,5,-3};
+        System.out.println(kadanesalgo(array));
+    }
+
+    public static int kadanesalgo(int array[]) {
+        boolean flag = false;
+        for(int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                flag = false;
+            }
+        }
+        if (flag == false) {
+            int max = Integer.MIN_VALUE;
+            for(int i = 0; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        for(int i = 0; i < array.length; i++) {
+            cs = cs + array[i];
+            if (cs <= 0) {
+                cs = 0;
+            }
+            ms = Math.max(ms, cs);
+        }
+        return ms;
+    }
+}
