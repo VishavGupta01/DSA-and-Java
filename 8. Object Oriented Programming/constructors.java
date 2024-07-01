00ae19723da4928cd13d453898e3b0a48c597c10
Vishav Gupta <singlasavita91@gmail.com>
2024-07-01 17:38:14 +0530

    Shallow Copy & Deep Copy

--- 8. Object Oriented Programming/constructors.java
+++ 8. Object Oriented Programming/constructors.java
@@ -2,13 +2,14 @@
 public class constructors {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.name = "Vishav";
-        s1.marks[0] = 100;
+        s1.rollNo = 5;
+        //s1.marks[0] = 100;
+
         Student s2 = new Student(s1);
-        s1.marks[0] = 90;
-        s1.name = "Vish";
-        System.out.println(s2.marks[0]);
-        System.out.println(s2.name);
+        s1.rollNo = 6;
+        //s1.marks[0] = 90;
+        System.out.println(s2.rollNo);
+        //System.out.println(s2.marks[0]);
     }
 }
 
@@ -35,7 +36,7 @@ class Student {
     }
     Student(String name1, int roll) {
         this.name = name1;
-        this.rollNo = roll;
+        this.rollNo = rollNo;
         marks = new int[3];
     }
 
@@ -44,7 +45,13 @@ class Student {
         marks = new int[3];
         this.name = s1.name;
         this.rollNo = s1.rollNo;
-        this.marks = s1.marks;
+        //Shallow Copy
+        //this.marks = s1.marks; //Copying the reference to other s1.marks array
+        //Deep Copy
+        this.marks = new int[3]; //Creating a new array for Deep Copy.
+        for(int i = 0; i < this.marks.length; i++) { // Storing all the values of s1.marks[] to s2.marks[]
+            this.marks[i] = s1.marks[i];
+        }
     }
 
 }
