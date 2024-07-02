4d4b14d06559cc2a5c650531d225426681067b3e
Vishav Gupta <singlasavita91@gmail.com>
2024-07-02 15:53:24 +0530

    Create packages.java

---
+++ 8. Object Oriented Programming/packages.java
@@ -0,0 +1,7 @@
+import java.util.*; //Utility Package
+
+public class packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Class objectName = new ConstructorName(Parameters);
+    }
+}
