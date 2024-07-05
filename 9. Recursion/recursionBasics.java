77696b34643bcc04023a166a51526e9d16efde66
Vishav Gupta <singlasavita91@gmail.com>
2024-07-05 17:16:18 +0530

    Update recursionBasics.java

--- 9. Recursion/recursionBasics.java
+++ 9. Recursion/recursionBasics.java
@@ -1,5 +1,14 @@
 public class recursionBasics {
     public static void main(String[] args) {
-        
+        printDec(10);
+    }
+
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printDec(n-1);
     }
 }
