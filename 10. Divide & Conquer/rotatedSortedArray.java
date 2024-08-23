44641d364c8da7b376c785325bceb19ed31384f2
Vishav Gupta <singlasavita91@gmail.com>
2024-08-23 21:57:16 +0530

    Update rotatedSortedArray.java

--- 10. Divide & Conquer/rotatedSortedArray.java
+++ 10. Divide & Conquer/rotatedSortedArray.java
@@ -2,7 +2,31 @@ public class rotatedSortedArray {
     public static void main(String[] args) {
         int[] array = {4,5,6,7,0,1,2};
         int key = 0;
-        System.out.println(rotatedSortedArraySearch(array, 0, array.length-1, 0));
+        //System.out.println(rotatedSortedArraySearch(array, 0, array.length-1, 0));
+
+        //By Iterations
+        int si = 0;
+        int ei = array.length-1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (array[mid] == key) {
+                System.out.println("Element found at " + mid);
+                break;
+            } else if (array[si] <= array[mid]) {
+                if (array[si] <= key && key <= array[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            } else {
+                if (array[mid] <= key && key <= array[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+        }
     }
 
     public static int rotatedSortedArraySearch(int[] arr, int si, int ei, int key) {
