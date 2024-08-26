0860f4ed5536cf15e96241d459b137499e45e795
Vishav Gupta <singlasavita91@gmail.com>
2024-08-26 15:34:08 +0530

    Update assignment10.java

--- 10. Divide & Conquer/assignment10.java
+++ 10. Divide & Conquer/assignment10.java
@@ -4,8 +4,17 @@ public class assignment10 {
         // mergeSort(arr, 0, arr.length-1);
         // printArray(arr);
 
-        int[] array = {2,2,1,1,1,2,2,1};
-        System.out.println(majority(array, 0, array.length-1));
+        // int[] array = {2,2,1,1,1,2,2,1};
+        // System.out.println(majority(array, 0, array.length-1));
+
+        int[] arr = {2, 4, 1, 3, 5};
+        System.out.println(inversionCount(arr, 0, arr.length-1));
+    }
+
+    public static void printArray1(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 
     public static void printArray(String[] array) {
@@ -102,6 +111,56 @@ public class assignment10 {
             if (array[i] == candidate) {
                 count++;
             }
+        }
+        return count;
+    }
+
+    //Question 3 :Given an array of integers. Find the Inversion Count in the array.
+    // Inversion Count:For an array, inversion count indicates how far (or close) the array is from being sorted.If the array is already sorted then the inversion count is 0.If an array is sorted in the reverse order then the inversion count is the maximum.Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.Sample Input 1: N = 5, arr[ ] = {2, 4, 1, 3, 5}Sample Output 1: 3, because it has 3 inversions -(2, 1), (4, 1), (4, 3).
+    // Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+    public static int inversionCount(int[] arr, int start, int end) {
+        if (start >=  end) {
+            return 0;
+        }
+        int count = 0;
+
+        int mid = start + (end - start)/2;
+        count += inversionCount(arr, start, mid);
+        count += inversionCount(arr, mid+1, end);
+        count += merge1(arr, start, mid, end);
+        return count;
+    }
+
+    public static int merge1(int[] arr, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start;
+        int j = mid+1;
+        int k = 0;
+        int count = 0;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                count += (mid - i + 1);
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j <= end) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(i = start, k = 0; k < temp.length; i++, k++) {
+            arr[i] = temp[k];
         }
         return count;
     }
