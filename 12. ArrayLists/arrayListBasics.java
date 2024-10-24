93407eed2746bce3c5e0b8ac658b2dc0b1ebd3e6
Vishav Gupta <singlasavita91@gmail.com>
2024-10-24 18:58:46 +0530

    Update arrayListBasics.java

--- 12. ArrayLists/arrayListBasics.java
+++ 12. ArrayLists/arrayListBasics.java
@@ -12,8 +12,24 @@ public class arrayListBasics {
         list.add(4);
         System.out.println(list);
 
+        // 1.5. Adding Elements at Index  // TC: O(n)
+        list.add(2,12);
+        System.out.println(list);
+
         // 2. Get Element  // TC: O(1)
         System.out.println(list.get(1));
         System.out.println(list.get(0));
+
+        // 3. Delete Element  // TC: O(n) {Linear because all the elements after the removed element have to be shifted.}
+        list.remove(2);
+        System.out.println(list);
+
+        // 4. Set Element at index  // TC: O(n)
+        list.set(0, 10);
+        System.out.println(list);
+
+        // 5. Contains element?  // TC: O(n)
+        System.out.println(list.contains(5));
+        System.out.println(list.contains(4));
     }
 }
