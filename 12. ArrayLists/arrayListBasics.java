241babb28af2e8b22afc0ebbdb4dd055246dbf1b
Vishav Gupta <singlasavita91@gmail.com>
2024-10-26 19:52:58 +0530

    Update arrayListBasics.java

--- 12. ArrayLists/arrayListBasics.java
+++ 12. ArrayLists/arrayListBasics.java
@@ -49,5 +49,25 @@ public class arrayListBasics {
         // Collections Sort (Reverse) [Descending Order]
         Collections.sort(list, Collections.reverseOrder());
         System.out.println(list);
+
+        //2D ArrayLists
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        mainList.add(list1);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+        mainList.add(list2);
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(7);
+        list3.add(8);
+        list3.add(9);
+        mainList.add(list3);
+        System.out.println(mainList);
+        System.out.println(mainList.get(0).get(0));
     }
 }
