8a3e2a4f4305ee54350d43ca530dcf26dc1488c7
Vishav Gupta <singlasavita91@gmail.com>
2024-10-27 20:44:18 +0530

    Update problemTargetSum.java

--- 12. ArrayLists/problemTargetSum.java
+++ 12. ArrayLists/problemTargetSum.java
@@ -5,7 +5,7 @@ public class problemTargetSum {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
         int target = 5;
-        System.out.println(solution1(list, target));
+        System.out.println(solution2(list, target));
     }
 
     public static ArrayList<Integer> solution1(ArrayList<Integer> list, int target) { // Brute Force - O(n^2)
@@ -14,6 +14,20 @@ public class problemTargetSum {
                 if (list.get(i) + list.get(j) == target) {
                     return new ArrayList<>(Arrays.asList(list.get(i),list.get(j)));
                 }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public static ArrayList<Integer> solution2(ArrayList<Integer> list, int target) { // Optimized (Two - Pointer) - O(n)
+        int lp = 0, rp = list.size()-1;
+        while(lp < rp) {
+            if ((list.get(lp) + list.get(rp)) == target) {
+                return new ArrayList<>(Arrays.asList(list.get(lp) , list.get(rp)));
+            } else if ((list.get(lp) + list.get(rp)) < target) {
+                lp++;
+            } else {
+                rp--;
             }
         }
         return new ArrayList<>();
