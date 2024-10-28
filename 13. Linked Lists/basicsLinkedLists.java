3eb16efbca56cc44e8a3173ee6d4afa25fc54176
Vishav Gupta <singlasavita91@gmail.com>
2024-10-28 20:15:13 +0530

    Create basicsLinkedLists.java

---
+++ 13. Linked Lists/basicsLinkedLists.java
@@ -0,0 +1,19 @@
+public class basicsLinkedLists {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args) {
+        basicsLinkedLists list = new basicsLinkedLists();
+        
+    }
+}
