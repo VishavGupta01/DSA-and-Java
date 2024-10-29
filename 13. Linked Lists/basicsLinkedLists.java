2235e01ad2e93ba945ca14924a1aaf22c5a72cbc
Vishav Gupta <singlasavita91@gmail.com>
2024-10-29 20:54:45 +0530

    Update basicsLinkedLists.java

--- 13. Linked Lists/basicsLinkedLists.java
+++ 13. Linked Lists/basicsLinkedLists.java
@@ -12,8 +12,37 @@ public class basicsLinkedLists {
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data) { // O(1)
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = tail = null;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
     public static void main(String[] args) {
         basicsLinkedLists list = new basicsLinkedLists();
-        
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addFirst(0);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
     }
 }
