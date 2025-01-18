446b8ae4937de9ed1e68e952fa38c03766870f7e
Vishav Gupta <singlasavita91@gmail.com>
2025-01-18 22:05:01 +0530

    Implemented Core Doubly Linked Lists

--- 13. Linked Lists/doublyLL.java
+++ 13. Linked Lists/doublyLL.java
@@ -27,6 +27,21 @@ public class doublyLL {
         head = temp;
     }
 
+    public void addLast(int data) {
+        if(head == null) {
+            addFirst(data);
+            return;
+        }
+        Node temp = new Node(data);
+        size++;
+        Node ptr = head;
+        while(ptr.next != null) {
+            ptr = ptr.next;
+        }
+        ptr.next = temp;
+        temp.prev = ptr;
+    }
+
     public void removeFirst() {
         if(head == null) {
             System.out.println("List UnderFlow!");
@@ -42,6 +57,27 @@ public class doublyLL {
         size--;
     }
 
+    public int removeLast() {
+        if(head == null) {
+            System.out.println("List Underflow!");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int data = head.data;
+            head = tail = null;
+            size--;
+            return data;
+        }
+        Node temp = head;
+        while(temp.next.next != null) {
+            temp = temp.next;
+        }
+        int data = temp.next.data;
+        temp.next = null;
+        size--;
+        return data;
+    }
+
     public void print() {
         Node temp = head;
         while(temp != null) {
@@ -49,6 +85,20 @@ public class doublyLL {
             temp = temp.next;
         }
         System.out.println("null");
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     public static void main(String[] args) {
