0f878d19f23433f94c703cb9ee02e2a4a1c53f5b
Vishav Gupta <singlasavita91@gmail.com>
2025-01-19 15:41:15 +0530

    Update stackPractice.java

--- 14. Stacks/stackPractice.java
+++ 14. Stacks/stackPractice.java
@@ -34,6 +34,26 @@ public class stackPractice {
         pushAtBottom(s, d);
     }
 
+    // Q4. Stock Span Problem
+    public static void stockSpan(int[] stocks, int[] span) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for(int i = 1; i < span.length; i++) {
+            int curr = stocks[i];
+            while(!s.isEmpty() && curr > stocks[s.peek()]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                span[i] = i - s.peek();
+            }
+            s.push(i);
+        }
+    }
+
     public static void main(String[] args) {
 
         // Q1. Push at Bottom of Stack
@@ -56,5 +76,12 @@ public class stackPractice {
         System.out.println(st1);
         reverseStack(st1);
         System.out.println(st1);
+
+        // Q4. Stock Span Problem
+        int[] stocks = {100, 80, 60, 70, 60, 85, 100};
+        int[] span = new int[stocks.length];
+        stockSpan(stocks, span);
+
+        for(int i = 0; i < span.length; i++) System.out.print(span[i] + " ");
     }
 }
