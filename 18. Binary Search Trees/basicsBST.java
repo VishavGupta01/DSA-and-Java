796a4af591f38a7dc7ea178aa2d760c5f3e2d1bd
Vishav Gupta <singlasavita91@gmail.com>
2025-08-18 23:01:23 +0530

    Update basicsBST.java

--- 18. Binary Search Trees/basicsBST.java
+++ 18. Binary Search Trees/basicsBST.java
@@ -36,12 +36,58 @@ public class basicsBST {
         }
     }
 
+    public static Node delete(Node root, int val) {
+        if(root == null) {
+            return null;
+        }
+        if(root.data < val) {
+            root.right = delete(root.right, val);
+        } else if(root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // Case 1: Leaf Node
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+            // Case 2: Single Node
+            if(root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 3: Two Nodes
+            Node nextSucc = root.right; // Inorder Successor
+            while(nextSucc.left != null) {
+                nextSucc = nextSucc.left;
+            }
+            root.data = nextSucc.data;
+            root.right = delete(root.right, nextSucc.data);
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if(root == null) {
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.println(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if(root.data < k1) {
+            printInRange(root.right, k1, k2);
+        } else if(root.data > k2) {
+            printInRange(root.left, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         int[] values = {5, 1, 3, 4, 2, 7};
         Node root = null;
         for(int val : values) {
             root = insert(root, val);
         }
-        System.out.println(search(root, 7));
+        printInRange(root, 2, 4);
     }
 }
