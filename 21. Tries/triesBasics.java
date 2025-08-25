1f72dd6f5ee5fd9ac7c7bfb9945ffbd816d75183
Vishav Gupta <singlasavita91@gmail.com>
2025-08-25 22:30:27 +0530

    Update triesBasics.java

--- 21. Tries/triesBasics.java
+++ 21. Tries/triesBasics.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class triesBasics {
     static class Node {
         Node[] children = new Node[26];
         boolean eow = false;
+        int freq;
 
         public Node() {
             for(Node n : children) {
                 n = null;
             }
+            freq = 1;
         }
     }
 
@@ -16,6 +20,8 @@ public class triesBasics {
             char ch = s.charAt(i);
             if(curr.children[ch - 'a'] == null) {
                 curr.children[ch - 'a'] = new Node();
+            } else {
+                curr.children[ch - 'a'].freq++;
             }
             curr = curr.children[ch - 'a'];
         }
@@ -50,6 +56,40 @@ public class triesBasics {
         return false;
     }
 
+    public static String prefixProblem(String s, Node root) { // Approach 1
+        StringBuilder sb = new StringBuilder();
+        Node curr = root;
+
+        for(char ch : s.toCharArray()) {
+            curr = curr.children[ch - 'a'];
+            sb.append(ch);
+            int count = 0;
+            for(int i = 0; i < 26; i++) {
+                if(curr.children[i] != null) {
+                    count++;
+                }
+            }
+            if(count == 1) {
+                return sb.toString();
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String prefixProblem2(String s, Node root) { // Approach 2 (using Freq)
+        Node curr = root;
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+            curr = curr.children[ch - 'a'];
+            sb.append(ch);
+            if(curr.freq == 1) {
+                break;
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         // Node root1 = new Node();
         // String[] strings = {"the", "a", "there", "their", "any", "thee"};
@@ -59,13 +99,28 @@ public class triesBasics {
 
         // System.out.println(search("thei", root1));
 
-        Node root2 = new Node();
-        String[] dict = {"i", "like", "sam", "samsung", "mobile", "ice"};
-        String key = "ilikesamsung";
+        // Node root2 = new Node();
+        // String[] dict = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        // String key = "ilikesamsung";
 
-        for(String s : dict) {
-            insertion(s, root2);
+        // for(String s : dict) {
+        //     insertion(s, root2);
+        // }
+        // System.out.println(wordBreak(key, root2));
+
+        String[] arr = {"a", "ant"};
+
+        Node root3 = new Node();
+        root3.freq = -1;
+        for(String s : arr) {
+            insertion(s, root3);
         }
-        System.out.println(wordBreak(key, root2));
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(String s : arr) {
+            ans.add(prefixProblem2(s, root3));
+        }
+        System.out.println(ans);
     }
 }
